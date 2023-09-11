9b20e724a598ac40712b15de12fb30665499f849
polemicos <nikitarakovich61@gmail.com>
2023-09-11 13:05:07 +0200

    pisun sluchilsya

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,22 +2,48 @@
 
 @section('title', $task->title)
 
-
 @section('content')
 
-<p>{{$task->description}}</p>
+<div class="mb-4">
+    <a class=""
+    href="{{ route('tasks.index') }}">← Go back to the main page</a>
+</div>
+
+
+<p class="mb-4 text-slate-700">{{$task->description}}</p>
 
 @if($task->long_description)
-    <p>{{$task->long_description}}
+    <p class="mb-4 text-slate-700">{{$task->long_description}}
 @endif
 
-<p>{{$task->created_at}}</p>
-<p>{{$task->updated_at}}</p>
+<p class="mb-4 text-sm text-slate-500">Created {{$task->created_at->diffFOrHumans()}} • Updated {{$task->updated_at->diffFOrHumans()}}</p>
 
+<p class="mb-4">
+    @if ($task->isChecked)
+        <span class="font-medium text-green-500">Completed</span>
+    @else
+        <span class="font-medium text-red-500">Not completed</span>
+    @endif
+</p>
 
-<form action="{{ route('tasks.destroy', ['task'=>$task->id]) }}" method="POST">
-    @csrf
-    @method('DELETE')
-    <button type="submit">Delete</button>
-</form>
+<div class="flex gap-2">
+    <a class="btn" href=" {{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+
+    <form action="{{ route('tasks.toggleComplete', ['task'=>$task]) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <button class="btn" type="submit">
+            Mark as {{ $task->isChecked ? 'not completed' : 'completed'}}
+        </button>
+
+    </form>
+
+    <form action="{{ route('tasks.destroy', ['task'=>$task]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button class="btn" type="submit">Delete</button>
+    </form>
+</div>
 @endsection
+
